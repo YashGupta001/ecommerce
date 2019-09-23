2e67d86774956677e19b6bac4f4701018f5557d0
Yash Gupta <yash.guptacs92@gmail.com>
2019-09-23 22:02:53 +0530

    starting of project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+
+
+  render(){
+   
+    return (
+      <div className="App">
+     Test
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
