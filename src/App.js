6eb44a9e6ac09f93bb80d950d9b0d4739e2174f0
Yash Gupta <yash.guptacs92@gmail.com>
2019-09-23 23:26:33 +0530

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import HomePage from './homepage';
 
 class App extends React.Component {
-
-
-  render(){
-   
-    return (
-      <div className="App">
-     Test
-      </div>
-    );
-  }
- 
+	render() {
+		return (
+			<div className="App">
+				<HomePage />
+			</div>
+		);
+	}
 }
 
 export default App;
