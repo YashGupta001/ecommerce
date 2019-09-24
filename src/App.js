cf0f6c69a43256cf8ff61ca55b88d9a0ca9fa8bd
Yash Gupta <yash.guptacs92@gmail.com>
2019-09-24 16:09:10 +0530

    Routing Starts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.js';
+import { Route, Switch } from 'react-router-dom';
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<HomePage />
-			</div>
-		);
-	}
+const HatsPage = () => (
+	<div>
+		<h1>Hats Page</h1>
+	</div>
+);
+
+function App() {
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/shop/hats" component={HatsPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
